5059af97dffffa34e93869960735bf159ceb1817
VisvaV <visvafelix2005@gmail.com>
2025-05-07 15:39:45 +0530

    using graphviz

--- micrograd.py
+++ micrograd.py
@@ -1,8 +1,14 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from graph import *
+
 class Value:
-    def __init__(self, data, _children = (), _op = ''):
+    def __init__(self, data, _children = (), _op = '', label = ''):
         self.data = data
         self._prev = set(_children)
         self._op = _op
+        self.label = label
     
     def __repr__(self):
         return f"Value = {self.data}"
@@ -15,6 +21,14 @@ class Value:
         out = Value(self.data * other.data, (self, other), '*')
         return out
 
-a = Value(-3.0)
-b = Value(5.0)
-print(a+b)
+a = Value(-3.0, label = 'a')
+b = Value(5.0, label = 'b')
+c = Value(10.0, label = 'c')
+e = a*b ; e.label = 'e'
+d = e + c ; d.label = 'd'
+f = Value(6.0, label = 'f')
+L = d * f ; L.label = 'L'
+
+dot = draw_dot(L)
+dot.render('graph_output', format='png', view=True)
+
